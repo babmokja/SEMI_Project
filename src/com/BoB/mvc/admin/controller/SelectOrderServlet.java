895782a9d1d1cf55417e72fbc0b84bb3beb124fd
son-sung-hun <tjdgns8482@gmail.com>
2021-05-18 20:34:23 +0900

    admin부분 수정 추가

--- src/com/BoB/mvc/admin/controller/SelectOrderServlet.java
+++ src/com/BoB/mvc/admin/controller/SelectOrderServlet.java
@@ -7,11 +7,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.BoB.mvc.admin.service.AdminService;
+
 @WebServlet("/admin/order")
 public class SelectOrderServlet extends HttpServlet {
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+	//	String currentPage = request.getParameter("currentPage");
+	//	int pageNo = 1;
+		
+	//	if(currentPage != null && !"".equals(currentPage)) {
+	//		pageNo = Integer.parseInt(currentPage);
+	//	}
+		
+	//	if(pageNo <= 0) {
+	//		pageNo = 1;
+	//	}
+		AdminService adminService = new AdminService();
+		int totalCount = adminService.selectTotalCount();
+		
+		System.out.println(totalCount);
 		
 		String path="";
 		path = "/WEB-INF/views/admin/index-order.jsp";
